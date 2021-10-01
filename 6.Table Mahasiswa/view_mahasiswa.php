df3cceb6f77aaa6b107b54397d308bcc8d2e6677
mrDev321 <90969118+mrDev321@users.noreply.github.com>
2021-10-01 23:40:14 +0700

    Update mahasiswa_view.php
    add stylng for model

--- 6.Table Mahasiswa/view_mahasiswa.php
+++ 6.Table Mahasiswa/view_mahasiswa.php
@@ -5,6 +5,45 @@
     <meta charset="UTF-8">
     <title>Tabel Mahasiswa</title>
     
+     <style>
+        /* ====GLOBAL STYLE==== */
+        body {
+            background-color: #F8F8F8;
+        }
+
+        div.container {
+            width: 960px;
+            padding: 10px 50px 50px;
+            background-color: white;
+            margin: 20px auto;
+
+            box-shadow: 1px 0px 10px, -1px 0px 10px;
+        }
+
+        h1 {
+            text-align: center;
+            font-family: Cambria, "Times New Roman", serif;
+            clear: both;
+        }
+
+        /* ======TABLE====== */
+        table {
+            border-collapse: collapse;
+            border-spacing: 0;
+            border: 1px black solid;
+            width: 100%
+        }
+
+        th,
+        td {
+            padding: 8px 15px;
+            border: 1px black solid;
+        }
+
+        tr:nth-child(2n+3) {
+            background-color: #F2F2F2;
+        }
+    </style>
    
 </head>
 
