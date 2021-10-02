094bdbeb0b26a48c36e98978788aeb2f44b02dc5
mrDev321 <90969118+mrDev321@users.noreply.github.com>
2021-10-02 09:23:08 +0700

    add intro about controller in MVC

--- 6.Table Mahasiswa/controller_mahasiswa.php
+++ 6.Table Mahasiswa/controller_mahasiswa.php
@@ -1,3 +1,6 @@
+//Controller adalah bagian MVC yang berisi alur logika program dan juga sebagai penghubung
+//antara model dengan view.
+
 <?php
 include 'model_mahasiswa.php';
 $isiTabelMahasiswa = getTableMahasiswa();
